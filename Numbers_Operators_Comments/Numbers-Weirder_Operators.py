41e71e933e6aed039087ce06c9fdc301186712eb
jacks <jacksonrdraper@gmail.com>
2020-01-01 18:03:26 -0500

    Documentation on exponentiation, modulo, and integer division

---
+++ Numbers_Operators_Comments/Numbers-Weirder_Operators.py
@@ -0,0 +1,16 @@
+# "**" = Exponentiation
+    # 2**3 = 8
+    # Square root: 49 ** 0.5 = 7 
+# "%" = Modulo
+    # Known as the remainder operator
+    # Takes two numbers, divides them into each other as many times as possible without being fractional, giving us the remainder.
+    # 10 % 3 = 1 -> 3 goes into 10 3 times, with 1 left over
+    # 25 % 4 = 1 -> 4 goes into 25 6 times, with 1 left over.
+    # Modulo commonly used to determine if numbers are even or odd. Will potentially use this operation.
+# "//" = Integer Division
+    # Integer division always rounds down
+    # 10/3 = 3.3333 but 10//3 rounds down to 3
+    # 6//7 = 0 
+# Quiz: 2+10*5+3*2 = 50 + 6 + 2 = 58
+# Quiz: 6*8/2**3-2*2 = 48/8 - 4 = 6.0 - 4 = 2.0
+# Quiz: 1/2*2#+1 = .5*2 = 1.0 -> Everything after the comment is not a part of the operation. 
