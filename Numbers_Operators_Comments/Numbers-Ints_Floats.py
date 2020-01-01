b3cc816edac8e5fd970e08f6cfcf0a33200ba468
jacks <jacksonrdraper@gmail.com>
2020-01-01 18:01:31 -0500

    Documentation on common mathematical operations

---
+++ Numbers_Operators_Comments/Numbers-Ints_Floats.py
@@ -0,0 +1,21 @@
+# Two main types of numbers: #
+    # Integers vs Floating Points
+# Integer:
+    # 4, 57, -10
+# Float:
+    # 6.1, 1.3333, 0.0
+# type() tells us if the number is a float or an integer
+    # type(9) int
+    # type(9.0) float
+# if any part of an expression is a float, the end result will be a float
+    # ex: 1+1.0 = 2.0
+################################################################
+## Commonly Used Math Operators ##
+# "+" = Addition
+# "-" = Subtraction
+# "*" = Multiplication
+# "/" = Division
+    # Division always returns float values
+    # 1/3 = Float (0.3333333333)
+    # 1+2*5/3 -> PEMDAS (Parentheses, Exponents Multiplication, Division, Addition, Subtraction)
+    # Common mathematical principles apply directly to Python mathematical operations
