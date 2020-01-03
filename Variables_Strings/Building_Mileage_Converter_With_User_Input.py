0a624bc045daeeaf8c26f32b4b72994d2b4b3fc6
jacks <jacksonrdraper@gmail.com>
2020-01-02 23:36:15 -0500

    Removed unnecessary commented function

--- Variables_Strings/Building_Mileage_Converter_With_User_Input.py
+++ Variables_Strings/Building_Mileage_Converter_With_User_Input.py
@@ -4,7 +4,6 @@
 
 print("How many kilometers did you run today?")
 kms = input() # Changes the prompt, waits for user to hit the return key. #Kilometers
-# print("Ok, you said " + kms) # String
 print(f"Ok, you have run {round(float(kms) / 1.609344, 2)} miles today") # Convert kms to a float to have accurate mathematical operations
 # Rounded value to 2 decimal places using the round function inside of the f-string
 # Note: we cannot use concatenation here because we will have errors regarding strings and floats together
