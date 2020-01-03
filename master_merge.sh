5c3641690c9f548707928818ca4331c600ef6873
jacks <jacksonrdraper@gmail.com>
2020-01-03 15:27:04 -0500

    Testing master_merge updates

--- master_merge.sh
+++ master_merge.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # Adding, Committing, and Pushing updates to Master for Modern Python 3 Github Repo
 
-echo "What file has been updated?"
-
-read varname
-
-git add $varname
+git add .
 
 read -p "What changes have been made?: " str1
 git commit -m  "$str1"
